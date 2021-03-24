b54d147c94d929485dbd4fa5ed4d3e0cc2fddfa0
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-03-24 22:35:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,18 +4,18 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speaker)
-  speaker.each do |name|
-    puts "Hello, my name is #{name}."
+  speaker.collect do |name|
+    badge_maker(name)
   end
 end
-badge_maker(name)
+
 def assign_rooms(speaker)
-  speaker.each.with_index(1) do |name, room|
-    puts "Hello, #{name}! You'll be assigned to room #{room}!"
+  speaker.collect.with_index(1) do |name, room|
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
 
 def printer(speaker)
   batch_badge_creator(speaker).each {|badges|  puts badges}
-  assign_rooms(speaker).each {|room_assignment| puts room_assignment}
+  assign_rooms(speaker).each {|room_assignment| puts {room_assignment}
 end
